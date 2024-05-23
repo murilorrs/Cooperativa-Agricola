30fdbe1cc6eb4d940371c4cd454b0754e89c7d4b
Murilo <murilorodriguesrrs@gmail.com>
2024-05-23 03:41:12 -0300

    update_carregamento.c

--- src/carregamento.c
+++ src/carregamento.c
@@ -6,33 +6,11 @@
 #define MAX_PATH_SIZE (MAX_FILENAME_SIZE + sizeof(PATH_PREFIX)) // Tamanho total da string. ex:(../data/cargas/DadosGraos-Carga1.txt) ||funcao sizeof retorna a quantidade de caracteres de uma string.
 
 
-void classeDeCarga (int umidade, int quantidadeDeAmostras, int idAmostra) {
-    int faixaA [100];
-    int faixaB [100];
-    int faixaC [100];
 
-    int quantidadeA = 0;
-    int quantidadeB = 0;
-    int quantidadeC = 0;
 
-    for(int i = 0; i < quantidadeDeAmostras; i++){
-        if(umidade >= 0 && umidade <= 8.5){
-            faixaA[i] = idAmostra;
-            quantidadeA ++;
-        }else if(umidade >= 8.6 && umidade <= 15){
-            faixaB[i] = idAmostra;
-            quantidadeB ++;
-        }else if(umidade >=16 && umidade <= 25) {
-            faixaC[i] = idAmostra;
-            quantidadeC++;
-        }
-    }
-    printf("\nvou imprimir as faixas\n");
-    printf("%d\n", faixaA[0]);
-    printf("%d\n", faixaB[0]);
-    printf("%d\n", faixaC[10]);
-    printf("cheguei ate aqui");
-}
+
+
+
 
 void gerarRelatorio(int protocolo, int quantidadeDeAmostras, int DD, int MM, float umidade, float pesoLimpo, int transgenico, int ID) {
     printf("\n%d\n", protocolo);
@@ -52,7 +30,7 @@ void carregamento() {
     char caminhoCompleto[MAX_PATH_SIZE]; // declarando um vetor que vai receber a string completa do caminho
 
     int ID, DD, MM, protocolo, quantidadeDeAmostras, tipo;
-    float peso, somatorioDoPesoDasImpurezas, somatorioDoPesoDasAmostras, percentualDeImpurezas, multUmidadePesoImpureza, diferencaPesoImpureza, percentualUmidade, somatorioDiferencaPesoImpureza, umidade; //variaveis que vamos usar pra fazer os calculos de PIC e GUC
+    float pesoGeralDaCarga; //variaveis que vamos usar pra fazer os calculos de PIC e GUC
 
     printf("Digite o nome do arquivo da carga:\n");
     fgets(nomeDoArquivo, MAX_FILENAME_SIZE, stdin); // Aqui eu usei fgets invés de scanf porque o scanf estava me retornando um "O" no final do nome do arquivo, aí o programa nao encontrava o caminho certo
@@ -73,46 +51,51 @@ void carregamento() {
         printf("\nDigite o mes do recebimento dessa carga:\n");
         scanf("%d", &MM);
 
-        fscanf(file, "%d%d%f%d%d", &ID, &protocolo, &peso, &quantidadeDeAmostras, &tipo); // Usa fscanf para ler os 5 primeiros dados do arquivo
-        // printf("\nID: %d\nProtocolo: %d\nPeso: %.2f\nQtd de Amostras: %d\nTipo: %d\n", ID, protocolo, peso, quantidadeDeAmostras, tipo);// apenas mostra no terminal
+        fscanf(file, "%d%d%f%d%d", &ID, &protocolo, &pesoGeralDaCarga, &quantidadeDeAmostras, &tipo); // Usa fscanf para ler os 5 primeiros dados do arquivo
+
+        // **********************************************************************LENDO OS PROXIMOS ARQUIVOS******************************************************************************************************************************
 
         char conteudoDoArquivo[200]; // declara um vetor que vai receber o conteúdo do arquivo => a unica finalidade é pra podermos usar o fgets mais pra frente, nao armazena dados relevantes
-        // inicializa as variáveis
-        somatorioDoPesoDasImpurezas = 0;
-        somatorioDoPesoDasAmostras = 0; 
-        multUmidadePesoImpureza = 0;
-        somatorioDiferencaPesoImpureza = 0;
-
         fgets(conteudoDoArquivo, 200, file); // Como a gente precisa ler a partir da segunda linha (os dados de impureza e umidade começam lá), lemos aqui a primeira linha para pulá-la no próximo fgets
+
+        // inicializa as variáveis que vao receber os valores de cada elemento da amostra
+        int idAmostra; // recebe o elemento 1 da linha
+        float pesoDaAmostra, pesoDaImpureza, umidade; // recebe o elemento 2 em diante da mesma linha
+
+        //variaveis de calculo PIC GUC
+        float somatorioDoPesoDasImpurezas = 0, somatorioDoPesoDasAmostras = 0, somatorioDiferencaPesoImpureza = 0, multiplicaUmidadePesoImpureza = 0, diferencaPesoImpureza;//variaveis usadas pra calcular PIC E GUC
+        float percentualDeImpurezas,percentualUmidade;//recebe valores de PIC E GUC
+
+        //variáveis para gerar o relatório
+        int quantidadeA = 0, quantidadeB = 0, quantidadeC = 0;
+        int faixaA [100];
+        int faixaB [100];
+        int faixaC [100];
+
 
         // faz um loop para ler linha a linha do arquivo até acabar
         while (fgets(conteudoDoArquivo, 200, file) != NULL) {
-            int idAmostra; // declaração do elemento 1 da linha
-            float pesoDaAmostra, pesoDaImpureza; // declaração dos elementos 2 em diante da mesma linha
 
             //O laço ta lendo linha por linha, toda vez que ele passa por uma linha esse sscanf armazena um dado da linha nas variaveis, nessa ordem do sscanf
             if (sscanf(conteudoDoArquivo, "%d %f %f %f", &idAmostra, &pesoDaAmostra, &pesoDaImpureza, &umidade) == 4) { //estou usando sscanf pq essa funcao além de funcionar como um fscanf normal, ela checa se os 4 valores foram lidos corretamente.
                 
                 diferencaPesoImpureza = pesoDaAmostra - (pesoDaImpureza/1000);
-                multUmidadePesoImpureza += umidade*diferencaPesoImpureza;
+                multiplicaUmidadePesoImpureza += umidade*diferencaPesoImpureza;
                 somatorioDiferencaPesoImpureza += pesoDaAmostra - (pesoDaImpureza/1000);
 
                 somatorioDoPesoDasImpurezas += (pesoDaImpureza/1000);
                 somatorioDoPesoDasAmostras += pesoDaAmostra;
-
-
-                
-
 
             } else {
                 printf("Nao foi possivel ler a linha do arquivo.\n");
             }
         }
 
+
         // gerarRelatorio(protocolo, quantidadeDeAmostras, DD, MM, umidade, peso, tipo, ID);
 
     percentualDeImpurezas = somatorioDoPesoDasImpurezas / somatorioDoPesoDasAmostras; 
-    percentualUmidade = multUmidadePesoImpureza / somatorioDiferencaPesoImpureza;
+    percentualUmidade = multiplicaUmidadePesoImpureza / somatorioDiferencaPesoImpureza;
 
     // printf("\nPercentual de Impureza:%.2f%%\n", percentualDeImpurezas*100);
     // printf("Percentual Umidade:%.2f\n", percentualUmidade);
