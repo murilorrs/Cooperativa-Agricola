73378fdccf834d9434ddfaea3d39fa91924c6680
murilorrs <murilorodriguesrrs@gmail.com>
2024-06-11 21:40:05 -0300

    update

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
+
+double media (double *vetor, int);
+
+
 int main(){
 
-    int notas [5];
-    int nota;
-    int maiorNota = 0;
+    double vetor[5];
+    double valor;
+    int tamanho = 5;
 
-    for(int i = 1; i < 6; i++){
-        printf("Digite a nota %d\n", i);
-        scanf("%d", &nota);
-
-        notas[i] = nota;
+    for(int i = 0; i < tamanho; i++){
+        printf("Digite o valor:\n");
+        scanf("%lf", &valor);
+        vetor[i] = valor;
     }
 
+    printf("A media é: %.2lf\n",media (vetor, tamanho));
 
     return 0;
+}
+
+double media (double *vetor, int tamanho){
+    double acumulador = 0;
+    double media;
+
+    for(int i = 0; i < tamanho; i++){
+        acumulador += *(vetor + i);
+        media = acumulador / tamanho;
+
+    }
+    return media;
 }
